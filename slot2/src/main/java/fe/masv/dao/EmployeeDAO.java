afe9b3a72cc7f1fa418e26cf30f2f2ce0ba5e71b
planhtai2005-web <planhtai2005@gmail.com>
2026-09-12 13:52:03 +0700

    feat(todo-03): implement employee save

---
+++ slot2/src/main/java/fe/masv/dao/EmployeeDAO.java
@@ -0,0 +1,39 @@
+﻿package fe.masv.dao;
+
+import fe.masv.pojo.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+public class EmployeeDAO {
+
+    private final EntityManagerFactory emf;
+
+    public EmployeeDAO(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    // TODO 0.3 - CREATE
+    public void save(Employee e) {
+
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+
+            em.persist(e);
+
+            em.getTransaction().commit();
+
+        } catch (Exception ex) {
+
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+
+            throw ex;
+
+        } finally {
+            em.close();
+        }
+    }
+}
