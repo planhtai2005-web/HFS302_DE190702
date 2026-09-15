c927da3e652d2520b15e7c3c0c2203ce41d6c11a
planhtai2005-web <planhtai2005@gmail.com>
2026-09-16 00:23:00 +0700

    feat(todo-02.3): map department employees

--- slot3/src/main/java/fe/masv/pojo/Department.java
+++ slot3/src/main/java/fe/masv/pojo/Department.java
@@ -1,6 +1,8 @@
 package fe.masv.pojo;
 
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "departments")
@@ -14,6 +16,11 @@ public class Department {
     private String name;
 
     private String location;
+
+    @OneToMany(mappedBy = "department",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private List<Employee> employees = new ArrayList<>();
 
     public Department() {
     }
@@ -45,5 +52,13 @@ public class Department {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
     }
 }
