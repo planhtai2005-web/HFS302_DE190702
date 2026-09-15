5ae330307140514e3ae2ca31cb869a8c1747c97a
planhtai2005-web <planhtai2005@gmail.com>
2026-09-16 00:31:18 +0700

    feat(todo-02.5): implement department DAO

---
+++ slot3/src/main/java/fe/masv/dao/DepartmentDAO.java
@@ -0,0 +1,94 @@
+package fe.masv.dao;
+
+import fe.masv.pojo.Department;
+import fe.masv.util.JPAUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+import java.util.List;
+
+public class DepartmentDAO {
+
+    public void save(Department department) {
+        EntityManager em = JPAUtil.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            em.persist(department);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Department> findAll() {
+        EntityManager em = JPAUtil.getEntityManager();
+
+        try {
+            return em.createQuery(
+                    "SELECT d FROM Department d",
+                    Department.class
+            ).getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Department findById(Long id) {
+        EntityManager em = JPAUtil.getEntityManager();
+
+        try {
+            return em.find(Department.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public void update(Department department) {
+        EntityManager em = JPAUtil.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            em.merge(department);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+
+    public void delete(Long id) {
+        EntityManager em = JPAUtil.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+
+            Department department = em.find(Department.class, id);
+
+            if (department != null) {
+                em.remove(department);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+}
