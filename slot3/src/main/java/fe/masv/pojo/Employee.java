09a5291f24524fb05c3cf92d3de7de4ce492ae81
planhtai2005-web <planhtai2005@gmail.com>
2026-09-16 00:19:13 +0700

    feat(todo-02.2): map employee department

--- slot3/src/main/java/fe/masv/pojo/Employee.java
+++ slot3/src/main/java/fe/masv/pojo/Employee.java
@@ -26,6 +26,11 @@ public class Employee {
 
     private boolean active;
 
+    // TODO 2.2 - ManyToOne relationship
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id", nullable = false)
+    private Department department;
+
     public Employee() {
     }
 
@@ -93,5 +98,14 @@ public class Employee {
 
     public void setActive(boolean active) {
         this.active = active;
+    }
+
+    // TODO 2.2 - Getter/Setter
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
     }
 }
